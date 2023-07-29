cb5ca09b4a01522b74755a7d8883ba5b10d1ad61
DanikTheDeveloper <69887442+DanikTheDeveloper@users.noreply.github.com>
2023-07-29 13:13:35 -0700

    design

--- miniProj.py
+++ miniProj.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 from tkinter import simpledialog, messagebox
-
 import sqlite3
+import winsound 
+
+def play_sound():
+    winsound.PlaySound('C:\\Windows\\Media\\Windows Information Bar.wav', winsound.SND_ASYNC)
+
 
 def create_connection():
     try:
@@ -13,6 +17,7 @@ def create_connection():
 
 
 def find_item():
+    play_sound()
     item_name = simpledialog.askstring("Find Item", "Enter Item Name:")
     conn = create_connection()
     cur = conn.cursor()
@@ -25,6 +30,7 @@ def find_item():
         messagebox.showerror("Error", "Item not found!")
 
 def borrow_item():
+    play_sound()
     userID = simpledialog.askinteger("Borrow Item", "Enter your User ID:")
     itemID = simpledialog.askinteger("Borrow Item", "Enter Item ID:")
     
@@ -36,6 +42,7 @@ def borrow_item():
     messagebox.showinfo("Success", f"Borrowed item {itemID}")
 
 def return_item():
+    play_sound()
     userID = simpledialog.askinteger("Return Item", "Enter your User ID:")
     itemID = simpledialog.askinteger("Return Item", "Enter Item ID to return:")
     
@@ -47,6 +54,7 @@ def return_item():
     messagebox.showinfo("Success", f"Returned item {itemID}")
 
 def donate_item():
+    play_sound()
     item_name = simpledialog.askstring("Donate Item", "Enter Item Name:")
     item_type = simpledialog.askstring("Donate Item", "Enter Item Type (e.g. book, CD, journal):")
     
@@ -58,6 +66,7 @@ def donate_item():
     messagebox.showinfo("Success", f"Thank you for donating {item_name}!")
 
 def find_event():
+    play_sound()
     event_name = simpledialog.askstring("Find Event", "Enter Event Name:")
     
     conn = create_connection()
@@ -71,12 +80,14 @@ def find_event():
         messagebox.showerror("Error", "Event not found!")
 
 def register_event():
+    play_sound()
     userID = simpledialog.askinteger("Register for Event", "Enter your User ID:")
     eventID = simpledialog.askinteger("Register for Event", "Enter Event ID to register:")
     # For simplicity, we're not adding a new table for event registration, but ideally, you'd want one.
     messagebox.showinfo("Success", f"Registered for event {eventID}")
 
 def ask_librarian():
+    play_sound()
     query = simpledialog.askstring("Ask a Librarian", "Enter your query:")
     # In a real-world scenario, this query would be stored and sent to librarians, but for now, we'll show a simple acknowledgment.
     messagebox.showinfo("Success", "Your query has been submitted! A librarian will get back to you soon.")
@@ -85,25 +96,40 @@ def main():
     root = tk.Tk()
     root.title("Library Application")
 
-    btn_find_item = tk.Button(root, text="Find Item", command=find_item)
+    # Design enhancements
+    root.configure(bg='#f0f8ff')  
+    lbl_title = tk.Label(root, text="Library Application", bg='#f0f8ff', font=("Arial", 24))
+    lbl_title.pack(pady=20)
+
+    button_config = {
+        'bg': '#007acc',
+        'fg': 'white',
+        'padx': 20,
+        'pady': 10,
+        'font': ('Arial', 12),
+        'activebackground': '#005da2',
+        'width': 20
+    }
+
+    btn_find_item = tk.Button(root, text="Find Item", command=find_item, **button_config)
     btn_find_item.pack(pady=10)
 
-    btn_borrow_item = tk.Button(root, text="Borrow Item", command=borrow_item)
+    btn_borrow_item = tk.Button(root, text="Borrow Item", command=borrow_item, **button_config)
     btn_borrow_item.pack(pady=10)
 
-    btn_return_item = tk.Button(root, text="Return Item", command=return_item)
+    btn_return_item = tk.Button(root, text="Return Item", command=return_item, **button_config)
     btn_return_item.pack(pady=10)
 
-    btn_donate_item = tk.Button(root, text="Donate Item", command=donate_item)
+    btn_donate_item = tk.Button(root, text="Donate Item", command=donate_item, **button_config)
     btn_donate_item.pack(pady=10)
 
-    btn_find_event = tk.Button(root, text="Find Event", command=find_event)
+    btn_find_event = tk.Button(root, text="Find Event", command=find_event, **button_config)
     btn_find_event.pack(pady=10)
 
-    btn_register_event = tk.Button(root, text="Register for Event", command=register_event)
+    btn_register_event = tk.Button(root, text="Register for Event", command=register_event, **button_config)
     btn_register_event.pack(pady=10)
 
-    btn_ask_librarian = tk.Button(root, text="Ask a Librarian", command=ask_librarian)
+    btn_ask_librarian = tk.Button(root, text="Ask a Librarian", command=ask_librarian, **button_config)
     btn_ask_librarian.pack(pady=10)
 
     root.mainloop()
